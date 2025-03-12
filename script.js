d9edb23ad7bb0ab042eee2399f9041b0df1ac86e
Somtoochukwu Okonkwo <98573016+HilaryOkonkwo@users.noreply.github.com>
2025-03-12 12:41:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ ScrollReveal().reveal(".header_image img", {
 
 ScrollReveal().reveal(".header_content div", {
     duration:1000,
-    delay:5000,
+    delay:3000,
 });
 
 ScrollReveal().reveal(".header_content h1", {
     ...scrollRevealOption,
-    delay:6000,
+    delay:5000,
 });
 ScrollReveal().reveal(".header_content p", {
     ...scrollRevealOption,
-    delay:6500,
+    delay:5500,
 });
 
 ScrollReveal().reveal(".deals_card", {
